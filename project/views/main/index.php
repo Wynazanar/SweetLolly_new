b9fc3ae168a03d4e781dbfaeef847be39daca900
Wynazanar <goldenfreddi322@gmail.com>
2026-08-25 09:18:48 +0300

    Update

--- project/views/main/index.php
+++ project/views/main/index.php
@@ -104,7 +104,7 @@
           <h3>Быстрый старт</h3>
           <p>Пошагово: версия, IP, добавление сервера и регистрация.</p>
         </a>
-        <a class="beauty-card" href="./page/games/">
+        <a class="beauty-card" href="./games/">
           <svg width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
             <path d="M19 2H5c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h11c2.76 0 5-2.24 5-5V4c0-1.1-.9-2-2-2m-7 16h-2v2H8v-2H6v-2h2v-2h2v2h2zm3 1c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1m2-2c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1m1-5H6V5h12z"></path>
           </svg>
@@ -118,7 +118,7 @@
           <h3>Форум</h3>
           <p>Новости, вопросы, баги, идеи, помощь и обсуждения игроков.</p>
         </a>
-        <a class="beauty-card" href="./fill/">
+        <a class="beauty-card" href="./candies/">
           <svg width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
 						<path d="M21 8H7c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h14c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1m-1 8c-1.1 0-2 .9-2 2h-8c0-1.1-.9-2-2-2v-4c1.1 0 2-.9 2-2h8c0 1.1.9 2 2 2z"></path>
 						<path d="M18 4H3c-.55 0-1 .45-1 1v11h2V6h14zm-4 8a2 2 0 1 0 0 4 2 2 0 1 0 0-4"></path>
